cf7c46eeaefb42980e002198365218b953222157
Gena <genada89@gmail.com>
2026-07-02 18:12:54 +0300

    add movies to json

--- services/movie.py
+++ services/movie.py
@@ -12,7 +12,7 @@ def get_movies(
     queryset = Movie.objects.all()
 
     if title:
-        queryset = queryset.filter(title__contains=title)
+        queryset = queryset.filter(title__icontains=title)
 
     if genres_ids:
         queryset = queryset.filter(genres__id__in=genres_ids)
@@ -20,7 +20,7 @@ def get_movies(
     if actors_ids:
         queryset = queryset.filter(actors__id__in=actors_ids)
 
-    return queryset.distinct()
+    return queryset.distinct().order_by("id")
 
 
 def get_movie_by_id(movie_id: int) -> Movie:
